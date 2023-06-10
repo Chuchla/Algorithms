95c90001424b37d02a367c2e816e9f169781b654
Jakub Chuchla <jacobchuchlaschooling@gmail.com>
2023-06-10 21:25:10 +0200

    Initial commit

---
+++ Utils/RandomGraph/RandomGraph.cpp
@@ -0,0 +1,8 @@
+//
+// Created by GS65 on 10.06.2023.
+//
+#include "RandomGraph.h"
+
+void GenerateUndirectedGraph(int vertexNumber, int edgeNumber, int graphsDensity){
+
+};
---
+++ Utils/RandomGraph/RandomGraph.h
@@ -0,0 +1,14 @@
+//
+// Created by GS65 on 10.06.2023.
+//
+
+#ifndef ALGORITHMS_RANDOMGRAPH_H
+#define ALGORITHMS_RANDOMGRAPH_H
+
+
+class RandomGraph {
+
+};
+
+
+#endif //ALGORITHMS_RANDOMGRAPH_H
